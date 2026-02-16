6dd4ee7d247c339b4776f21bb5b3a406c5755ead
Viktor Johansson <discovicke@gmail.com>
2026-02-16 17:07:00 +0100

    implement basic Sudoku solving structure with validation logic

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -5,5 +5,45 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        var board = new int[9][];
+    }
+
+    static bool SolveSudoku(int[][] board)
+    {
+        return true;
+    }
+
+    bool isValid(int[][] board, int row, int col, int num)
+    {
+        // Is 'num' in current row?
+        for (var x = 0; x < 9; x++)
+        {
+            if (board[row][x] == num)
+            {
+                return false;
+            }
+        }
+
+        // Is 'num' in current column?
+        for (var x = 0; x < 9; x++)
+        {
+            if (board[x][col] == num)
+            {
+                return false;
+            }
+        }
+
+        // Is 'num' in current 3x3 box?
+        int startRow = row - row % 3, startCol = col - col % 3;
+        for (var i = 0; i < 3; i++)
+        {
+            for (var j = 0; j < 3; j++)
+            {
+                if (board[i + startRow][j + startCol] == num)
+                    return false;
+            }
+        }
+
+        return true;
     }
 }
