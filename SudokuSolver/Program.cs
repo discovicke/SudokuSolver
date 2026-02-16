2b5f300156d24dcc926cf646e61755fdafd33da7
Viktor Johansson <discovicke@gmail.com>
2026-02-16 17:45:49 +0100

    initialize Sudoku board and add starting values for solver

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -4,8 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
         var board = new int[9][];
+        for (var i = 0; i < 9; i++)
+        {
+            board[i] = new int[9];
+        }
+
+        board[0] = [5, 3, 0, 0, 7, 0, 0, 0, 0];
+        board[1] = [6, 0, 0, 1, 9, 5, 0, 0, 0];
+        board[2] = [0, 9, 8, 0, 0, 0, 0, 6, 0];
+        board[3] = [8, 0, 0, 0, 6, 0, 0, 0, 3];
+        board[4] = [4, 0, 0, 8, 0, 3, 0, 0, 1];
+        board[5] = [7, 0, 0, 0, 2, 0, 0, 0, 6];
+        board[6] = [0, 6, 0, 0, 0, 0, 2, 8, 0];
+        board[7] = [0, 0, 0, 4, 1, 9, 0, 0, 5];
+        board[8] = [0, 0, 0, 0, 8, 0, 0, 7, 9];
+
+        Console.WriteLine("Starting Sudoku Solver");
+        SolveSudoku(board);
+        
     }
 
     static bool SolveSudoku(int[][] board)
